aad43a90bb024780b4f80af5ffcf8421640ef618
chengde.tan <chengde.tan@dmall.com>
2021-03-22 22:06:47 +0800

    添加依赖注入作业相关内容

--- user-platform/user-web/src/main/java/org/geektimes/projects/user/management/UserMBeanDemo.java
+++ user-platform/user-web/src/main/java/org/geektimes/projects/user/management/UserMBeanDemo.java
@@ -1,5 +1,6 @@
 package org.geektimes.projects.user.management;
 
+import com.sun.jmx.mbeanserver.Introspector;
 import org.geektimes.projects.user.domain.User;
 
 import javax.management.MBeanServer;
@@ -11,14 +12,13 @@ public class UserMBeanDemo {
     public static void main(String[] args) throws Exception {
         // 获取平台 MBean Server
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-
         // 为 UserMXBean 定义 ObjectName
-        ObjectName objectName = new ObjectName("jolokia:type=User");
+        ObjectName objectName = new ObjectName("org.geektimes.projects.user.management:type=User");
         // 创建 UserMBean 实例
         User user = new User();
         mBeanServer.registerMBean(createUserMBean(user), objectName);
         while (true) {
-            Thread.sleep(5000);
+            Thread.sleep(2000);
             System.out.println(user);
         }
     }
